b3f054e7ca934e9da39c5709e2636f4d72024135
Andrey Nikolaychuk <60553738+Click-73@users.noreply.github.com>
2021-04-10 14:29:27 +0300

    Add files via upload

--- 21/12/index.js
+++ 21/12/index.js
@@ -1,10 +1,10 @@
 export const squaredNumbers = () => {
   const atribute = document.querySelector(".number");
-  [...atribute].map((item) => {
+  const newAtribute = [...atribute].map((item) => {
     const numberAtribute = item.dataset.number;
     const square = numberAtribute * numberAtribute;
     item.dataset.squaredNumber = square;
     return item;
   });
-  return atribute;
+  return newAtribute;
 };
