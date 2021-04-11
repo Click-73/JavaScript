53bbec8e21b71e0ba96117f573eeffcf80f91519
Andrey Nikolaychuk <60553738+Click-73@users.noreply.github.com>
2021-04-11 20:18:17 +0200

    Update index.js

--- 21/2/index.js
+++ 21/2/index.js
@@ -13,13 +13,13 @@
 // };
 
 export const getTitleElement = () => {
-  const titleElem = document.querySelector(".title");
+  const titleElem = document.querySelector('.title');
   console.dir(titleElem);
   return titleElem;
-};
+}
 
 export const getInputElement = () => {
   const inputElem = document.querySelector('[type="text"]');
   console.dir(inputElem);
   return inputElem;
-};
+}
