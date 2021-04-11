3fef2d7f7e325ff9ee9474a521c16778a9592dba
Andrey Nikolaychuk <60553738+Click-73@users.noreply.github.com>
2021-04-11 20:19:25 +0200

    Update index.js

--- 21/2/index.js
+++ 21/2/index.js
@@ -1,25 +1,26 @@
-// export const getTitleElement = () => {
-//   const titleElem = document.querySelector(".title");
-//   console.dir(titleElem);
-//   return titleElem;
-// };
-
-// export const getInputElement = () => {
-//   const inputElem = document
-//     .querySelector("input")
-//     .getAttribute("type", "text");
-//   console.dir(inputElem);
-//   return inputElem;
-// };
 
 export const getTitleElement = () => {
-  const titleElem = document.querySelector('.title');
+  const titleElem = document.querySelector(".title");
   console.dir(titleElem);
   return titleElem;
 }
 
 export const getInputElement = () => {
-  const inputElem = document.querySelector('[type="text"]');
+  const inputElem = document
+    .querySelector("input")
+    .getAttribute("type", "text");
   console.dir(inputElem);
   return inputElem;
 }
+
+// export const getTitleElement = () => {
+//   const titleElem = document.querySelector('.title');
+//   console.dir(titleElem);
+//   return titleElem;
+// }
+
+// export const getInputElement = () => {
+//   const inputElem = document.querySelector('[type="text"]');
+//   console.dir(inputElem);
+//   return inputElem;
+// }
