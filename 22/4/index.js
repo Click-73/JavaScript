96c10433480de28218d8373531ba430eb8b6c5d2
Andrey Nikolaychuk <60553738+Click-73@users.noreply.github.com>
2021-04-15 10:27:43 +0300

    Add files via upload

--- 22/4/index.js
+++ 22/4/index.js
@@ -1,8 +1,10 @@
 const checkBoxElem = document.querySelector(".task-status");
 
 const logCheckBox = () => {
-  if (checkBoxElem.checked) return console.log("true");
-  return console.log("false");
+  if (checkBoxElem.checked) console.log(true);
+  else {
+    console.log(false);
+  }
 };
 
 checkBoxElem.addEventListener("change", logCheckBox);
