64c4dd68913f20db9739fdd771473b4348d4044a
Andrey Nikolaychuk <60553738+Click-73@users.noreply.github.com>
2021-04-18 14:33:02 +0200

    Delete 24/1/1 directory

--- 24/1/1/index.js
+++
@@ -1,9 +0,0 @@
-export const dayOfWeek = (date, days) => {
-  const daysOfTheWeek = ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"];
-  const day = new Date(date).getDate();
-  const dateInFuture = new Date(date).setDate(day + days);
-
-  return daysOfTheWeek[new Date(dateInFuture).getDay()];
-};
-
-dayOfWeek(new Date(233333), 11);
