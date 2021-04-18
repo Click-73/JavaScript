9963defa60788f1a77305b131e000bf5744457b8
Andrey Nikolaychuk <60553738+Click-73@users.noreply.github.com>
2021-04-18 15:38:40 +0300

    Add files via upload

--- 24/1/index.js
+++ 24/1/index.js
@@ -1,9 +1,10 @@
+const daysOfTheWeek = ["Su", "Mo", "Tu", "We", "Th", "Fr", "St"];
+
 export const dayOfWeek = (date, days) => {
-  const daysOfTheWeek = ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"];
   const day = new Date(date).getDate();
-  const dateInFuture = new Date(date).satDate(day + days);
+  const dateInFuture = new Date(date).setDate(day + days);
 
   return daysOfTheWeek[new Date(dateInFuture).getDay()];
 };
 
-dayOfWeek(new Date(233333), 10);
+dayOfWeek(new Date(2019, 1, 1), 11);
