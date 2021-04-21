0cf7d9640ca288af8a1c52754b1092a7c2a9b45a
Andrey Nikolaychuk <60553738+Click-73@users.noreply.github.com>
2021-04-21 13:48:26 +0300

    Add files via upload

--- 24/4 Students push month/index.js
+++ 24/4 Students push month/index.js
@@ -1,4 +1,4 @@
-export const studentsDate = [
+const studentsDate = [
   {
     name: "Sam",
     birthDate: "03/15/2010",
@@ -15,19 +15,25 @@ export const studentsDate = [
 
 const studentsBirthDays = (students) => {
   const formatter = new Intl.DateTimeFormat("en", { month: "short" });
+  if (!Array.isArray(students)) {
+    return;
+  }
 
   const sortMonthOfBored = [...students]
     .sort(
       (a, b) =>
-        new Date(a.birthDate).getMonth() - new Date(b.birthDate).getMonth()
+        new Date(a.birthDate).getMonth() - new Date(b.birthDate).getMonth() ||
+        new Date(a.birthDate).getFullYear() -
+          new Date(b.birthDate).getFullYear()
     )
-    .reduce((acc, item) => {
-      item.birthDate = formatter.format(new Date(item.birthDate));
-      // console.log(item.birthDate);  month
-      // console.log(acc[item.birthDate]); names
 
-      acc[item.birthDate] = acc[item.birthDate] || [];
-      acc[item.birthDate].push(item.name);
+    .reduce((acc, elem) => {
+      const months = formatter.format(new Date(elem.birthDate));
+      // console.log(month); month
+      // console.log(acc[elem.birthDate]); names
+
+      acc[months] = acc[months] || [];
+      acc[months].push(elem.name);
       return acc;
     }, {});
 
